5dfb97b1e1b4cc2f0e26e590ea88a97e3e543436
everson.choma <everson.choma@gmail.com>
2023-05-30 20:58:42 -0300

    feat: add prisma and tables Game and Ad

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,25 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([]);
+})
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+})
+
+app.get('/games/:id/ads', (request, response) => {
     return response.json([
         { id: 1, name: 'anúncio 1'},
         { id: 2, name: 'anúncio 2'},
         { id: 3, name: 'anúncio 3'},
     ]);
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+    return response.json([]);
 })
 
 app.listen(3333)
