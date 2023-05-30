46d4cbe83aeacd5c04ee79e1512cc9c8de805326
everson.choma <everson.choma@gmail.com>
2023-05-30 11:03:36 -0300

    feat: add web landing page

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,9 +2,19 @@
 export default {
   content: [
     './src/**/*.tsx',
+    './index.html',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background.png')",
+        gradient: 'linear-gradient(89.86deg, #9572FC 20.00%, #43E7AD 65.00%, #E1D55D 20.00%)',
+        'game-gradient': 'linear-gradient(188deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.88%)',
+      }
+    },
   },
   plugins: [],
 }
