4029e1ff78d91b9d8277d6e166ca118604a83347
everson.choma <everson.choma@gmail.com>
2023-06-01 20:53:13 -0300

    feat: add form dialog and styles

--- web/src/components/CreateAddBanner.tsx
+++ web/src/components/CreateAddBanner.tsx
@@ -1,6 +1,7 @@
 import { MagnifyingGlassPlus } from "phosphor-react";
+import * as Dialog from "@radix-ui/react-dialog";
 
-export function CreateaddBanner() {
+export function CreateAddBanner() {
   return (
     <div className="pt-1 bg-gradient self-stretch rounded-lg overflow-hidden  mt-8">
       <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
@@ -13,10 +14,10 @@ export function CreateaddBanner() {
           </span>
         </div>
 
-        <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+        <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
           <MagnifyingGlassPlus size={24} />
           Publicar anúncio
-        </button>
+        </Dialog.Trigger>
       </div>
     </div>
   );
